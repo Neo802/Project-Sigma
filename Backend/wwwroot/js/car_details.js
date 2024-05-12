aebcc3fa272d8e4a850f1f9fa692abe335eecff9
Albert <gamescellrblx@protonmail.com>
2024-05-12 17:15:23 +0300

    Updated Car view, so that you could see a more detailed description

--- Backend/wwwroot/js/car_details.js
+++ Backend/wwwroot/js/car_details.js
@@ -2,7 +2,14 @@
     fetch(`/Cars/GetCarDetails?id=${carId}`)
         .then(response => response.json())
         .then(data => {
+            document.getElementById('carName').innerText = data.manufacturer; // Make sure your data object has a description property
+            document.getElementById('carModel').innerText = data.model; // Make sure your data object has a description property
+            document.getElementById('carImage').pic = data.image; // Make sure your data object has a description property
             document.getElementById('carDetails').innerText = data.description; // Make sure your data object has a description property
+            document.getElementById('fuel').innerText = data.fuel; // Make sure your data object has a description property
+            document.getElementById('fuelCapacity').innerText = data.tankCapacity; // Make sure your data object has a description property
+            document.getElementById('gearType').innerText = data.gear; // Make sure your data object has a description property
+            document.getElementById('doorsCount').innerText = data.doors; // Make sure your data object has a description property
             document.getElementById('carDetailsModal').style.display = 'flex'; // Show the modal
         })
         .catch(error => console.error('Error:', error));
